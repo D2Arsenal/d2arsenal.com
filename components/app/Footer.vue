2c45a62886863f0e950aab1a646befaa491500a5
Alexander Lichter <github@lichter.io>
2022-10-28 11:18:23 +0200

    feat(mobile): footer

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -5,15 +5,21 @@ const year = new Date().getFullYear()
 const { version } = pkg
 </script>
 <template>
-  <footer class="flex justify-between mx-4 mb-2 text-gray-500">
-    <div>
-      © {{ year }} - D2 Arsenal v{{ version }}
-      &bull;
-      Discord
-      &bull;
-      Twitter
-      &bull;
-      Donate
+  <footer class="flex flex-col space-y-2 sm:space-y-0 sm:flex-row justify-between mx-4 mb-2 text-gray-500">
+    <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0">
+      <span>© {{ year }} - D2 Arsenal v{{ version }}</span>
+      <span>
+        <span class="hidden sm:inline">
+        &bull;
+      </span>
+      <span>
+        Discord
+        &bull;
+        Twitter
+        &bull;
+        Donate
+      </span>
+      </span>
     </div>
     <div>Destiny 2 & all related media © Bungie</div>
   </footer>
