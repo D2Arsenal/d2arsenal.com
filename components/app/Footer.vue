dd546a7ba04eeaada33b2eec6d5d55002d8220f1
Alexander Lichter <github@lichter.io>
2022-11-06 19:48:55 +0100

    chore: footer spacing

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -7,11 +7,9 @@ const { version } = pkg
 <template>
   <footer class="flex flex-col space-y-2 sm:space-y-0 sm:flex-row justify-between mx-4 mb-2 text-gray-500">
     <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0">
-      <span>© {{ year }} - D2 Arsenal v{{ version }}</span>
-      <span>
-        <span class="hidden sm:inline">
-        &bull;
-      </span>
+      <span>© {{ year }} - D2 Arsenal v{{ version }}<span class="hidden sm:inline">
+        &bull;&nbsp;
+      </span></span>
       <span>
         <NuxtLink href="/discord/" target="_blank">
           Discord
@@ -24,7 +22,6 @@ const { version } = pkg
         <NuxtLink to="/donate/">
           Donate
         </NuxtLink>
-      </span>
       </span>
     </div>
     <div>Destiny 2 & all related media © Bungie</div>
