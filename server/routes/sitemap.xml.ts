b6eb9f93cd3316c06d382e77a41664556db88bf3
Alexander Lichter <github@lichter.io>
2022-11-09 10:33:45 +0100

    fix(sitemap): set header correctly

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -8,7 +8,7 @@ const SITE_URL = config.public.siteUrl
 
 const BASE_URLS = ['/', '/donate/']
 
-export default defineCachedEventHandler(async () => {
+export default defineCachedEventHandler(async (event) => {
   const minimalWeapons = await $fetch<MinimalWeapon[]>(joinURL(SITE_URL, '/api/weapons'))
   const urls = BASE_URLS.concat(minimalWeapons.map(w => `/en/weapons/${w.hash}/`))
 
@@ -23,6 +23,8 @@ export default defineCachedEventHandler(async () => {
     })
   })
   sitemap.end()
+
+  setHeader(event, 'content-type', 'application/xml')
 
   return streamToPromise(sitemap)
 }, { maxAge: 0 })
