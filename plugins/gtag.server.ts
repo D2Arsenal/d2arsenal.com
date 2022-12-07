a8f634ebb0244dfbbea8cc4f592266e1a04566ff
Alexander Lichter <github@lichter.io>
2022-12-07 19:49:25 +0100

    chore: fix typo

--- plugins/gtag.server.ts
+++ plugins/gtag.server.ts
@@ -2,18 +2,17 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
   const id = config.gtagId
 
-  const preloadLinks = [
-    {
-      re1l: 'preload',
-      as: 'script',
-      href: `https://www.googletagmanager.com/gtag/js?id=${id}`,
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://www.google-analytics.com/',
-    },
-  ]
   nuxtApp._useHead({
-    link: preloadLinks,
+    link: [
+      {
+        rel: 'preload',
+        as: 'script',
+        href: `https://www.googletagmanager.com/gtag/js?id=${id}`,
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://www.google-analytics.com/',
+      },
+    ],
   })
 })
